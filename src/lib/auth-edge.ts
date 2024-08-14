88e3543eaf33fd0f69e89f1778f82753f3ac77e2
hannatrush <h4nnatrush@gmail.com>
2024-08-14 09:27:00 +0300

    remove bcrypt from edge

--- src/lib/auth-edge.ts
+++ src/lib/auth-edge.ts
@@ -1,43 +1,10 @@
-import NextAuth, { NextAuthConfig } from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
-import bcrypt from 'bcryptjs';
-import { getUserByEmail } from './server-utils';
-import { authSchema } from './validations';
+import { NextAuthConfig } from 'next-auth';
+import prisma from './db';
 
-const config = {
+export const nextAuthEdgeConfig = {
   pages: {
     signIn: '/login',
   },
-  providers: [
-    Credentials({
-      async authorize(credentials) {
-        // runs on login
-
-        // validation
-        const validatedFormData = authSchema.safeParse(credentials);
-
-        if (!validatedFormData.success) {
-          return null;
-        }
-
-        const { email, password } = validatedFormData.data;
-
-        const user = await getUserByEmail(email);
-        if (!user) {
-          console.log('No user found');
-          return null;
-        }
-
-        const passwordsMatch = await bcrypt.compare(password, user.hashedPassword);
-        if (!passwordsMatch) {
-          console.log('Invalid password');
-          return null;
-        }
-
-        return user;
-      },
-    }),
-  ],
   callbacks: {
     authorized: ({ auth, request }) => {
       const isLoggedIn = Boolean(auth?.user);
@@ -88,7 +55,11 @@ const config = {
       }
 
       if (trigger === 'update') {
-        const userFromDb = await getUserByEmail(token.email);
+        const userFromDb = await prisma.user.findUnique({
+          where: {
+            email: token.email,
+          },
+        });
         if (userFromDb) {
           token.hasAccess = userFromDb.hasAccess;
         }
@@ -103,11 +74,5 @@ const config = {
       return session;
     },
   },
+  providers: [],
 } satisfies NextAuthConfig;
-
-export const {
-  auth,
-  signIn,
-  signOut,
-  handlers: { GET, POST },
-} = NextAuth(config);
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,7 @@
-import { NextResponse } from 'next/server';
-import { auth } from './lib/auth';
+import NextAuth from 'next-auth';
+import { nextAuthEdgeConfig } from './lib/auth-edge';
 
-export default auth;
-// export function middleware(request: Request) {
-//   return NextResponse.next();
-// }
+export default NextAuth(nextAuthEdgeConfig).auth;
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
